492eb19466ab13dde3db192877724f20f0dabf6d
Aleksandr <90009560+KozlovAleksandr@users.noreply.github.com>
2022-04-03 00:15:06 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
-import { MessageList, Layout, ChatList, Header } from "./components";
-
 import "./global.css";
 
 ReactDOM.render(
