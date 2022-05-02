181c54bd7589fe7e8402e98041fe429572d3d9c8
Aleksandr <90009560+KozlovAleksandr@users.noreply.github.com>
2022-05-02 17:50:22 +0300

    api

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Header } from "../../components";
-import { HomePage, ChatPage, ProfilePage } from "../../pages";
+import { HomePage, ChatPage, ProfilePage, GistsPage } from "../../pages";
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/chat/*" element={<ChatPage />}></Route>
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/gists" element={<GistsPage />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </BrowserRouter>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import { profileReducer } from "./profile";
 import { conversationReducer } from "./conversations";
 import { messageReducer } from "./messages";
-import { botMessage, thunk } from "./middlewares";
+import { botMessage } from "./middlewares";
+
+import { getPublicGistsApi, searchGistsByNameApi } from "../api/gists";
+import { gistsReducer } from "./gists";
 
 const persistConfig = {
   key: "root",
@@ -18,11 +22,17 @@ const reducer = combineReducers({
   profile: profileReducer,
   conversations: conversationReducer,
   messages: messageReducer,
+  gists: gistsReducer,
 });
 
 export const store = createStore(
   persistReducer(persistConfig, reducer),
-  compose(applyMiddleware(thunk, botMessage))
+  compose(
+    applyMiddleware(
+      thunk.withExtraArgument({ getPublicGistsApi, searchGistsByNameApi }),
+      botMessage
+    )
+  )
 );
 
 export const persistor = persistStore(store);
