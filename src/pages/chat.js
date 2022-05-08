00c5c4e2a89b3de3827beb4b67e6d8c9e2b3bd31
Aleksandr <90009560+KozlovAleksandr@users.noreply.github.com>
2022-05-09 00:57:13 +0300

    firebase database

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,9 +1,36 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { EmptyChat } from "./emptyChat";
+import { getConversations } from "../store/conversations";
+import { getMessages } from "../store/messages";
 
 import { Layout, ChatBlock, ContactsBlock } from "../components";
 
 export const ChatPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const listener = ({ code }) => {
+      if (code === "Escape") {
+        navigate("/chat");
+      }
+    };
+
+    document.addEventListener("keydown", listener);
+
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [navigate]);
+
+  useEffect(() => {
+    dispatch(getConversations());
+    dispatch(getMessages());
+  }, [dispatch]);
+
   return (
     <>
       <Routes>
